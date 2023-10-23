6643af3fd269b17a63962bf79b838560b18e718c
Rossi Meacham <rossimeacham@gmail.com>
2023-10-23 15:57:27 -0400

    changes

--- components/LightSidebar.js
+++ components/LightSidebar.js
@@ -10,6 +10,7 @@ import {
   UsersIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 const navigation = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
@@ -31,6 +32,7 @@ function classNames(...classes) {
 
 export default function Example(props) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  console.log(props);
 
   return (
     <>
@@ -136,32 +138,38 @@ export default function Example(props) {
                         </li>
                         <li>
                           <div className="text-xs font-semibold leading-6 text-gray-400">
-                            Your teams
+                            Channels
                           </div>
                           <ul role="list" className="-mx-2 mt-2 space-y-1">
-                            {teams.map((team) => (
-                              <li key={team.name}>
-                                <a
-                                  href={team.href}
-                                  className={classNames(
-                                    team.current
-                                      ? "bg-gray-50 text-indigo-600"
-                                      : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                  )}
+                            {props.channels.map((channel) => (
+                              <li key={channel.slug}>
+                                <Link
+                                  legacyBehavior
+                                  href="/channels/[id]"
+                                  as={`/channels/${channel.id}`}
                                 >
-                                  <span
-                                    className={classNames(
-                                      team.current
-                                        ? "text-indigo-600 border-indigo-600"
-                                        : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
-                                      "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
+                                  <a className={classNames(
+                                      channel.id == props.activeChannelId
+                                        ? "bg-gray-50 text-indigo-600"
+                                        : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                                      "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                     )}
                                   >
-                                    {team.initial}
-                                  </span>
-                                  <span className="truncate">{team.name}</span>
-                                </a>
+                                    <span
+                                      className={classNames(
+                                        channel.id == props.activeChannelId
+                                          ? "text-indigo-600 border-indigo-600"
+                                          : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
+                                        "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
+                                      )}
+                                    >
+                                      {channel.slug.slice(0, 1).toUpperCase()}
+                                    </span>
+                                    <span className="truncate">
+                                      {channel.slug}
+                                    </span>
+                                  </a>
+                                </Link>
                               </li>
                             ))}
                           </ul>
@@ -218,32 +226,37 @@ export default function Example(props) {
                 </li>
                 <li>
                   <div className="text-xs font-semibold leading-6 text-gray-400">
-                    Your teams
+                    Channels
                   </div>
                   <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <a
-                          href={team.href}
-                          className={classNames(
-                            team.current
-                              ? "bg-gray-50 text-indigo-600"
-                              : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                          )}
+                    {props.channels.map((channel) => (
+                      <li key={channel.slug}>
+                        <Link
+                          legacyBehavior
+                          href="/channels/[id]"
+                          as={`/channels/${channel.id}`}
                         >
-                          <span
+                          <a
                             className={classNames(
-                              team.current
-                                ? "text-indigo-600 border-indigo-600"
-                                : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
-                              "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
+                              channel.id == props.activeChannelId
+                                ? "bg-gray-50 text-indigo-600"
+                                : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                              "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                             )}
                           >
-                            {team.initial}
-                          </span>
-                          <span className="truncate">{team.name}</span>
-                        </a>
+                            <span
+                              className={classNames(
+                                channel.id == props.activeChannelId
+                                  ? "text-indigo-600 border-indigo-600"
+                                  : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
+                                "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
+                              )}
+                            >
+                              {channel.slug.slice(0, 1).toUpperCase()}
+                            </span>
+                            <span className="truncate">{channel.slug}</span>
+                          </a>
+                        </Link>
                       </li>
                     ))}
                   </ul>
